e4d3b00cb6e24ef6f6dc4766952fc9b459f1a523
TheOneWhoMeasures <foravnovel@gmail.com>
2021-11-29 16:44:47 +0800

    template_again

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as Sort from "./scripts/sorting.js"
 
 
-window.onLoad = fetch("chapters/metadata.json")
+/*window.onLoad = fetch("chapters/metadata.json")
 .then(rsp => rsp.json())
-.then(data => Sort.chapters(data))
+.then(data => Sort.chapters(data))*/
